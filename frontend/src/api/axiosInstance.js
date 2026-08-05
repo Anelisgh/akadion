c709c21f7fcd2a83b4d9a1db5667dafd31b17bf6
Boteanu Ștefania Roxana <boteanustefania@yahoo.com>
2026-08-05 14:10:18 +0300

    fix: sanitize all backend response urls globally via Axios interceptor

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -8,4 +8,39 @@ const axiosInstance = axios.create({
   xsrfHeaderName: "X-XSRF-TOKEN",
 })
 
+function sanitizeUrls(obj) {
+  if (obj === null || obj === undefined) {
+    return obj
+  }
+  if (typeof obj === "string") {
+    if (obj.includes("http://minio:9000")) {
+      return obj.replace(/http:\/\/minio:9000/g, "http://localhost:9000")
+    }
+    return obj
+  }
+  if (Array.isArray(obj)) {
+    return obj.map(sanitizeUrls)
+  }
+  if (typeof obj === "object") {
+    const res = {}
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        res[key] = sanitizeUrls(obj[key])
+      }
+    }
+    return res
+  }
+  return obj
+}
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      response.data = sanitizeUrls(response.data)
+    }
+    return response
+  },
+  (error) => Promise.reject(error)
+)
+
 export default axiosInstance
